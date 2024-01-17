758af0a8c226d67dae57c1b852ea6198bf41d20d
Anan Widianto <anan.widi1@gmail.com>
2024-01-17 21:06:12 +0700

    improvement: get all user

--- module/feature/user/domain/response.go
+++ module/feature/user/domain/response.go
@@ -2,15 +2,18 @@ package domain
 
 import (
 	"ruti-store/module/entities"
+	"time"
 )
 
 type UserResponse struct {
-	ID           uint64 `json:"id"`
-	Email        string `json:"email"`
-	Password     string `json:"-"`
-	Phone        string `json:"phone"`
-	Name         string `json:"name"`
-	PhotoProfile string `json:"photo_profile"`
+	ID           uint64    `json:"id"`
+	Email        string    `json:"email"`
+	Password     string    `json:"-"`
+	Phone        string    `json:"phone"`
+	Name         string    `json:"name"`
+	PhotoProfile string    `json:"photo_profile"`
+	Role         string    `json:"role"`
+	CreatedAt    time.Time `json:"created_at"`
 }
 
 func UserFormatter(user *entities.UserModels) *UserResponse {
@@ -50,6 +53,8 @@ func ResponseArrayUser(data []*entities.UserModels) []*UserResponse {
 			Phone:        userItem.Phone,
 			Name:         userItem.Name,
 			PhotoProfile: userItem.PhotoProfile,
+			Role:         userItem.Role,
+			CreatedAt:    userItem.CreatedAt,
 		}
 		res = append(res, userRes)
 	}
